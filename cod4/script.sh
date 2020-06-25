8d783ef3cf5054dfe0e50e44c3239a0d62d1cdc4
Henrik S Nielsen <1741035+henkall@users.noreply.github.com>
2020-06-25 23:11:48 +0200

    Update script.sh

--- cod4/script.sh
+++ cod4/script.sh
@@ -16,19 +16,16 @@ fi
 
 if [ "$(ls -A gamefiles/mods)" ]; then
      echo "Mods Good"
-     chmod 777 gamefiles/mods
 else
     echo "ERROR Mods is Empty"
 fi
 if [ "$(ls -A gamefiles/usermaps)" ]; then
      echo "Usermaps Good"
-     chmod 777 gamefiles/usermaps
 else
     echo "ERROR Usermaps is Empty"
 fi
 if [ "$(ls -A gamefiles/zone)" ]; then
      echo "Zone is Good"
-     chmod 777 gamefiles/zone
 else
     echo "ERROR Zone is Empty"
 fi
@@ -72,10 +69,12 @@ if [[ ! -z "${READY}" ]]; then
 	echo "Config is Ready"
 	if [[ ! -z "${MODNAME}" ]]; then
 		echo "Mod enabled (using $MODNAME mod)"
-		gamefiles/cod4x18_dedrun "+set dedicated $SERVERTYPE" "+set net_port $PORT" "+set fs_game mods/$MODNAME" "$EXTRA" "+exec $EXECFILE" "$MAP"
+		cd gamefiles
+		./cod4x18_dedrun "+set dedicated $SERVERTYPE" "+set net_port $PORT" "+set fs_game mods/$MODNAME" "$EXTRA" "+exec $EXECFILE" "$MAP"
 	else
 		echo "Not using Mod"
-		gamefiles/cod4x18_dedrun "+set dedicated $SERVERTYPE" "+set net_port $PORT" "$EXTRA" "+exec $EXECFILE" "$MAP"
+		cd gamefiles
+		./cod4x18_dedrun "+set dedicated $SERVERTYPE" "+set net_port $PORT" "$EXTRA" "+exec $EXECFILE" "$MAP"
 	fi
 
 fi
