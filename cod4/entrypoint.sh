d4b0320ba3d40014a09fe0536a26c09a59d6b3ef
Henrik S Nielsen <1741035+henkall@users.noreply.github.com>
2021-08-03 17:17:18 +0200

    making userpermissions

--- cod4/entrypoint.sh
+++ cod4/entrypoint.sh
@@ -3,6 +3,26 @@
 source /etc/apache2/envvars
 #tail -F /var/log/apache2/* &
 
+# Changing www-data user id
+useridtest=$(id -u www-data)
+    if [[ $useridtest -eq ${PUID} ]]
+        then
+           echo "Id is fine"
+    else
+           usermod -u ${PUID} www-data
+           echo "change id"
+    fi
+
+# Changing www-data group id
+groupidtest=$(id -g www-data)
+    if [[ $groupidtest -eq ${PGID} ]]
+        then
+           echo "Id is fine"
+    else
+           usermod -g ${PGID} www-data
+           echo "change id"
+    fi
+
 foldergamefiles=$(stat --format '%a' /root/gamefiles)
     if [[ $foldergamefiles -eq 2777 ]] || [[ $folderzone -eq 777 ]]
         then
@@ -12,5 +32,6 @@ foldergamefiles=$(stat --format '%a' /root/gamefiles)
            chmod -R 2777 /root/gamefiles
            echo "fix applied to gamefiles ----"
     fi
+
 
 exec apache2 -D FOREGROUND
